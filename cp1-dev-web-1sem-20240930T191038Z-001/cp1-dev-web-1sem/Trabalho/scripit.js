a9e90cb066017cab807503ba0c062897b1b2285b
Marcelo Affonso Fonseca <marcelo.anonimo2006@gmail.com>
2024-09-30 16:14:44 -0300

    Add files via upload

---
+++ cp1-dev-web-1sem-20240930T191038Z-001/cp1-dev-web-1sem/Trabalho/scripit.js
@@ -0,0 +1,31 @@
+let x = null;
+console.log(x)
+let y;
+console.log(y)
+var nome = "teste";
+console.log(nome)
+
+let numero = "10.40"
+console.log(parseFloat(numero));
+let vertipo = typeof numero
+console.log(vertipo)
+
+let numero2 = 200
+console.log(parseFloat(numero2))
+let vertipo2 = typeof numero2
+console.log(vertipo2)
+
+let numero3 = 654.27
+console.log(parseInt(numero3))
+let vertipo3 = typeof numero3
+console.log(vertipo3)
+
+let nome = "Kayque Carvalho"
+let email = "kayque@gmail.com"
+let idade ="18"
+let telefone = "(11) 9 9999-9999"
+
+document.write("Nome:"+nome+"<br>")
+document.write("Idade:"+idade+"<br>")
+document.write("E-mail:"+email+"<br>")
+document.write("Telefone:"+telefone+"<br>")
